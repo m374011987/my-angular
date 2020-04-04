9ce902e7e5b9a6026657b0bbe7cdd492ebb7e772
m374011987 <374011987@qq.com>
2020-04-04 23:21:23 +0800

    添加message服务类及其对应组件

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  //note: 注入messageservice,获取英雄列表就对其数组进行添加
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
