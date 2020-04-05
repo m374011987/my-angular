3ca3bebfcf98fe23c5f09676dde5eb27e96be451
m374011987 <374011987@qq.com>
2020-04-05 17:53:40 +0800

    添加路由和outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { HerosComponent } from './heros/heros.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HerosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  //note: 这里的exports有什么用,为了app.module.ts中支持importAppRoutingModule吗?
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
