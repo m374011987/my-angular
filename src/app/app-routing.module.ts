0de5c0f3dd401838e1ad8a517fe85a0912981024
m374011987 <374011987@qq.com>
2020-04-06 13:13:52 +0800

    添加dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HerosComponent } from './heros/heros.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'heroes', component: HerosComponent }
+  { path: 'heroes', component: HerosComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
