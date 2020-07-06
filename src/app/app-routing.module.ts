019f41b0b3dd362778f208b3a020edcbadd15ae6
m374011987 <374011987@qq.com>
2020-07-06 21:47:45 +0800

    初始化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HerosComponent } from './heros/heros.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [
-  { path: 'heroes', component: HerosComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  //note: 这里的exports有什么用,为了app.module.ts中支持importAppRoutingModule吗?
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MyModuleModule } from './my-module/my-module.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HerosComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    MyModuleModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
